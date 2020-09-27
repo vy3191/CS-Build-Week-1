b5c072b488dc20eebd343bc9a2647f9908833edd
vy3191 <venkyafhs@gmail.com>
2020-09-27 12:07:57 -0400

    completed simulation function

--- src/Board.js
+++ src/Board.js
@@ -3,6 +3,16 @@ import produce from 'immer';
 
 const numberOfRows = 25;
 const numberOfColumns = 25;
+const operations = [
+  [0,1],
+  [0,-1],
+  [1,-1],
+  [-1,1],
+  [1,1],
+  [-1,-1],
+  [1,0],
+  [-1,0]
+];
 
 function Board() {
   const [running, setRunning] = useState(false);
@@ -16,11 +26,34 @@ function Board() {
   })
   const runSimulation = useCallback(() =>{
     // if we are not running return 
-    if(!running) {
+    if(!runningRef.current) {
       return;
     }
+    setGrid((g) => {
+      return produce(g, gridCopy => {
+        for(let i=0; i<numberOfRows; i++) {
+          for(let j=0; j<numberOfColumns; j++) {
+            let neighbors = 0;
+            operations.forEach(([x,y]) => {
+                const newI = i+x;
+                const newJ = j+y;
+                if(newI >= 0 && newI < numberOfRows && newJ>=0 && newJ < numberOfColumns) {
+                      neighbors += g[newI][newJ]
+                }
+            });
+            if(neighbors < 2 || neighbors > 3) {
+              gridCopy[i][j] = 0;
+            }else if(g[i][j] === 0 && neighbors === 3) {
+              gridCopy[i][j] = 1;
+            }
+          }
+        }
+      });
+    });
+    
     setTimeout(runSimulation, 1000)
   },[]);
+
   console.log(grid);
   return (
     <div className="container">
